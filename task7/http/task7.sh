9e92d495e9544cad7f6777659c385eecf32eb017
ceccs17d13 <43635320+ceccs17d13@users.noreply.github.com>
2019-05-04 18:25:32 +0530

    Add files via upload

---
+++ task7/http/task7.sh
@@ -0,0 +1,7 @@
+#!/bin/sh                                                                         
+                                                            
+sudo apt-get install nginx
+sudo /usr/sbin/nginx
+ sudo ufw app list
+sudo gedit /etc/nginx/sites-enabled/default
+sudo gedit /var/www/html/index.html
